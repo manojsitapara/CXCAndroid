0781c5a6effe45b059bf6cdce21b5c7faf63d74a
Manoj Sitapara <manoj.sitapara@hotmail.com>
2017-06-11 17:18:07 +0530

    Add project files.

---
+++ CXC.ClaimXO.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+using System.Threading.Tasks;
+
+namespace CXC.ClaimXO.Android
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string Tag = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(Tag, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Log.Debug(Tag, "Performing some startup work that takes a bit of time.");
+            await Task.Delay(2000); // Simulate a bit of startup work.
+            Log.Debug(Tag, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
